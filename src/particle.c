4250837f5b8c84ef6ef728a8a0c8dc3e95c87141
luisclaudio26 <luisclaudiogouveiarocha@gmail.com>
2015-07-03 19:22:35 +0200

    initial implementation of spring()

--- src/particle.c
+++ src/particle.c
@@ -2,26 +2,33 @@
 #include <math.h>
 
 #define PRINTPAIR(label,pair) printf("%s = (%f,%f)\n", label, pair.x, pair.y);
+#define PI 3.14
 
-//-----------------------------------
-//------------ INTERNAL -------------
-//-----------------------------------
+//-----------------------------------------------------
+//------------ INTERNAL - VECTORIAL STUFF -------------
+//-----------------------------------------------------
 typedef struct {
 	double mod, ang;
 } Polar2D;
 
-static Polar2D distance(Particle* p1, Particle* p2)
+static Polar2D difference(Particle* p1, Particle* p2)
 {
 	Polar2D out;
 	Vec2D diff;
 
-	diff.x = p1->pos.x - p2->pos.x;
-	diff.y = p1->pos.y - p2->pos.y;
+	diff.x = p2->pos.x - p1->pos.x;
+	diff.y = p2->pos.y - p1->pos.y;
 
 	out.mod = Vec2D_modulus(diff);
 	out.ang = Vec2D_angle(diff);
 
 	return out;
+}
+
+static void polar2Rect(Polar2D src, Vec2D* dst)
+{
+	dst->x = src.mod * cos(src.ang);
+	dst->y = src.mod * sin(src.ang);
 }
 
 double Vec2D_modulus(Vec2D v)
@@ -35,15 +42,41 @@ double Vec2D_angle(Vec2D v)
 }
 
 //-------------------------------------------
+//------------ INTERNAL - OTHER -------------
+//-------------------------------------------
+void spring(Particle* p1, Particle* p2, 
+			double springLength, double springCoeff, double LoE)
+{
+	//Calculate deformation
+	Polar2D diff = difference(p1, p2);
+	double deformation = springLength - diff.mod;
+
+	//Calculate elastic force
+	Polar2D elasticForce_p;
+	elasticForce_p.mod = springCoeff * deformation;
+	elasticForce_p.ang = diff.ang;
+
+	//Convert to rectangular coordinates
+	Vec2D elasticForce_r; 
+	polar2Rect(elasticForce_p, &elasticForce_r);
+
+	//Apply forces : particle 2 receives the force
+	//in opposite direction (as our distance vector
+	//points from p1 to p2)
+	p1->force.x += elasticForce_r.x;
+	p1->force.y += elasticForce_r.y;
+
+	p2->force.x += -elasticForce_r.x;
+	p2->force.y += -elasticForce_r.y;
+}
+
+//-------------------------------------------
 //------------ FROM PARTICLE.H --------------
 //-------------------------------------------
 void interact(Particle* p1, Particle* p2)
 {
-
-
-
-
-	return;
+	//Test values - change this for some constant after
+	spring(p1, p2, 1.0, 1.0, 1.0);
 }
 
 void initializeParticle(Particle* p, double mass, double radius)
