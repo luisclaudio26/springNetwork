676870d58c07a141e7ebab1fc250bf1bff9400ae
luisclaudio26 <luisclaudiogouveiarocha@gmail.com>
2015-07-04 12:19:31 +0200

    + initialize and display particle grid

--- src/springNetwork.c
+++ src/springNetwork.c
@@ -1,38 +1,44 @@
 #include "../include/springNetwork.h"
 #include "../include/parameters.h"
 
-static Particle P1, P2;
+#define GRID_WIDTH 10
+#define GRID_HEIGHT 10
+
+static Particle P[GRID_WIDTH][GRID_HEIGHT];
 
 //-------------------------------------------------
 //------------- FROM SPRINGNETWORK.H --------------
 //-------------------------------------------------
 void drawNetwork()
 {
-	//printf("Drawing\n");
-
-	drawPoint(P1.pos.x, P1.pos.y, P1.radius, YELLOW);
-	drawPoint(P2.pos.x, P2.pos.y, P2.radius, RED);
+	Particle* aux = &P;
+	for(int i = 0; i < GRID_WIDTH * GRID_HEIGHT; i++, aux++)
+		drawPoint(aux->pos.x, aux->pos.y, aux->radius, YELLOW);
 }
 
 void updateNetwork(double timeStep)
 {
 	//printf("Updating\n");
 
+	/*
 	interact(&P1, &P2);
 
 	updateParticle(&P1, timeStep);
 	updateParticle(&P2, timeStep);
+	*/
 }
 
 void initializeNetwork()
 {
 	printf("Initializing network...\n");
 
-	initializeParticle(&P1, 10.0, 1.0);
-	initializeParticle(&P2, 15.0, 1.0);
-
-	P1.pos.x = P1.pos.y = 100;
-	P2.pos.x = P2.pos.y = 300;
+	for(int i = 0; i < GRID_WIDTH; i++)
+		for(int j = 0; j < GRID_HEIGHT; j++)
+		{
+			initializeParticle(&P[i][j], 10.0, 4.0);
+			P[i][j].pos.x = i * 20 + 50;
+			P[i][j].pos.y = j * 20 + 50;
+		}
 }
 
 void destroyNetwork()
